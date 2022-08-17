15d1c21accf98e91fc4a61e549e2f50df672c06e
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-17 13:09:42 -0500

    v.0.11.1 Se integra form para crear nomina

--- views/nom_nomina/crea_nomina.php
+++ views/nom_nomina/crea_nomina.php
@@ -7,6 +7,20 @@
 
         <div class="  form-main" id="form">
             <form method="post" action="./index.php?seccion=org_empresa&accion=modifica_cif&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+                <?php echo $controlador->inputs->select->org_sucursal_id; ?>
+                <?php echo $controlador->inputs->select->em_empleado_id; ?>
+                <?php echo $controlador->inputs->rfc; ?>
+                <?php echo $controlador->inputs->curp; ?>
+                <?php echo $controlador->inputs->nss; ?>
+                <?php echo $controlador->inputs->folio; ?>
+                <?php echo $controlador->inputs->fecha; ?>
+                <?php echo $controlador->inputs->select->cat_sat_tipo_nomina_id; ?>
+                <?php echo $controlador->inputs->num_dias_pagados; ?>
+                <?php echo $controlador->inputs->salario_diario; ?>
+                <?php echo $controlador->inputs->salario_diario_integrado; ?>
+                <?php echo $controlador->inputs->fecha_pago; ?>
+                <?php echo $controlador->inputs->fecha_inicial_pago; ?>
+                <?php echo $controlador->inputs->fecha_final_pago; ?>
 
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
--- views/nom_nomina/modifica.php
+++ views/nom_nomina/modifica.php
@@ -18,18 +18,15 @@ $url_icons = (new views())->url_icons;
                 <?php echo $controlador->inputs->codigo; ?>
                 <?php echo $controlador->inputs->codigo_bis; ?>
                 <?php echo $controlador->inputs->descripcion; ?>
-                <?php echo $controlador->inputs->version; ?>
-                <?php echo $controlador->inputs->serie; ?>
-                <?php echo $controlador->inputs->folio; ?>
-                <?php echo $controlador->inputs->fecha; ?>
-                <?php echo $controlador->inputs->tipo_cambio; ?>
-                <?php echo $controlador->inputs->exportacion; ?>
+                <?php echo $controlador->inputs->num_dias_pagados; ?>
+                <?php echo $controlador->inputs->fecha_pago; ?>
+                <?php echo $controlador->inputs->fecha_inicial_pago; ?>
+                <?php echo $controlador->inputs->fecha_final_pago; ?>
                 <?php echo $controlador->inputs->select->em_empleado_id; ?>
-                <?php echo $controlador->inputs->select->org_sucursal_id; ?>
+                <?php echo $controlador->inputs->select->cat_sat_tipo_nomina_id; ?>
+                <?php echo $controlador->inputs->select->fc_factura_id; ?>
                 <?php echo $controlador->inputs->select->dp_calle_pertenece_id; ?>
-                <?php echo $controlador->inputs->select->cat_sat_moneda_id; ?>
-                <?php echo $controlador->inputs->select->cat_sat_metodo_pago_id; ?>
-                <?php echo $controlador->inputs->select->cat_sat_tipo_de_comprobante_id; ?>
+                <?php echo $controlador->inputs->select->im_registro_patronal_id; ?>
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
                         <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
