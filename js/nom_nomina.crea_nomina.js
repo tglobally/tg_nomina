2516c37d2804aa57456de4222166e32779139c4b
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-19 13:44:39 -0500

    v.0.13.1 Se integra alta para crear nomina

---
+++ js/nom_nomina.crea_nomina.js
@@ -0,0 +1,107 @@
+
+let sl_nom_empleado = $("#em_empleado_id");
+let sl_cat_sat_periodicidad_pago_nom = $("#cat_sat_periodicidad_pago_nom_id");
+
+
+let txt_rfc = $('#rfc');
+let txt_curp = $('#curp');
+let txt_nss = $('#nss');
+let txt_salario_diario = $('#salario_diario');
+let txt_salario_diario_integrado = $('#salario_diario_integrado');
+let txt_fecha_inicio_rel_laboral = $('#fecha_inicio_rel_laboral');
+let txt_num_dias_pagados = $('#num_dias_pagados');
+let txt_fecha_inicial_pago = $('#fecha_inicial_pago');
+let txt_fecha_final_pago = $('#fecha_final_pago');
+let txt_subtotal = $('#subtotal');
+
+sl_nom_empleado.change(function(){
+    let selected = $(this).find('option:selected');
+
+    let rfc = selected.data('em_empleado_rfc');
+    let curp = selected.data('em_empleado_curp');
+    let nss = selected.data('em_empleado_nss');
+    let salario_diario = selected.data('em_empleado_salario_diario');
+    let salario_diario_integrado = selected.data('em_empleado_salario_diario_integrado');
+    let fecha_inicio_rel_laboral  = selected.data('em_empleado_fecha_inicio_rel_laboral');
+
+    txt_rfc.val(rfc);
+    txt_curp.val(curp);
+    txt_nss.val(nss);
+    txt_salario_diario.val(salario_diario);
+    txt_salario_diario_integrado.val(salario_diario_integrado);
+    txt_fecha_inicio_rel_laboral.val(fecha_inicio_rel_laboral);
+
+    let fechaInicioRelLaboral = new Date(txt_fecha_inicio_rel_laboral.val());
+    let fechaInicialPago = new Date(txt_fecha_inicial_pago.val());
+
+    if(fechaInicioRelLaboral > fechaInicialPago){
+        txt_fecha_inicial_pago.val(fecha_inicio_rel_laboral);
+    }
+
+    let sub_Total = subTotal(txt_salario_diario.val(),txt_num_dias_pagados.val())
+    txt_subtotal.val(sub_Total)
+});
+
+sl_cat_sat_periodicidad_pago_nom.change(function(){
+    let selected = $(this).find('option:selected');
+    let num_dias_pagados = selected.data('cat_sat_periodicidad_pago_nom_n_dias');
+
+    let fechaFinal
+
+    if (selected.val() !== '') {
+         fechaFinal = fecha(txt_fecha_inicial_pago, num_dias_pagados)
+    } else {
+         fechaFinal = fecha(txt_fecha_inicial_pago)
+    }
+    txt_num_dias_pagados.val(num_dias_pagados);
+    txt_fecha_final_pago.val(fechaFinal)
+    let sub_Total = subTotal(txt_salario_diario.val(),txt_num_dias_pagados.val())
+    txt_subtotal.val(sub_Total)
+});
+
+txt_fecha_inicial_pago.change(function() {
+    let selected = sl_cat_sat_periodicidad_pago_nom.find('option:selected');
+    let num_dias_pagados = selected.data('cat_sat_periodicidad_pago_nom_n_dias');
+
+    let fechaFinal
+
+    if (selected.val() !== '') {
+        fechaFinal = fecha(txt_fecha_inicial_pago, num_dias_pagados)
+    } else {
+        num_dias_pagados = (txt_num_dias_pagados.val() !== '') ? txt_num_dias_pagados.val() : 1
+        fechaFinal = fecha(txt_fecha_inicial_pago, num_dias_pagados)
+    }
+    txt_fecha_final_pago.val(fechaFinal)
+});
+
+txt_num_dias_pagados.change(function() {
+    let selected = sl_cat_sat_periodicidad_pago_nom.find('option:selected');
+    let num_dias_pagados = selected.data('cat_sat_periodicidad_pago_nom_n_dias');
+
+    let fechaFinal
+
+    if (selected.val() !== '') {
+        fechaFinal = fecha(txt_fecha_inicial_pago, num_dias_pagados)
+    } else {
+        num_dias_pagados = (txt_num_dias_pagados.val() !== '') ? txt_num_dias_pagados.val() : 1
+        fechaFinal = fecha(txt_fecha_inicial_pago, num_dias_pagados)
+    }
+    txt_fecha_final_pago.val(fechaFinal)
+    let sub_Total = subTotal(txt_salario_diario.val(),txt_num_dias_pagados.val())
+    txt_subtotal.val(sub_Total)
+});
+
+let fecha = (fechaInicio, numDias = 1) => {
+
+    var fechaInicial = new Date(fechaInicio.val());
+    var fechaFinal   = new Date(fechaInicio.val());
+    var dias = parseInt(numDias);
+
+    fechaFinal.setDate(fechaInicial.getDate() + dias);
+    return fechaFinal.getFullYear() + "-" +  ('0' + (fechaFinal.getMonth()+1)).slice(-2) + "-" + ('0' + fechaFinal.getDate()).slice(-2);
+};
+
+let subTotal = (salario = 0, diasPagados = 0,) => {
+   return salario * diasPagados
+};
+
--- views/nom_nomina/crea_nomina.php
+++ views/nom_nomina/crea_nomina.php
@@ -6,22 +6,24 @@
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
 
         <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=org_empresa&accion=modifica_cif&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
-                <?php echo $controlador->inputs->select->org_sucursal_id; ?>
+            <form method="post" action="./index.php?seccion=nom_nomina&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+                <?php echo $controlador->inputs->select->im_registro_patronal_id; ?>
                 <?php echo $controlador->inputs->select->em_empleado_id; ?>
                 <?php echo $controlador->inputs->rfc; ?>
                 <?php echo $controlador->inputs->curp; ?>
                 <?php echo $controlador->inputs->nss; ?>
                 <?php echo $controlador->inputs->folio; ?>
                 <?php echo $controlador->inputs->fecha; ?>
+                <?php echo $controlador->inputs->fecha_inicio_rel_laboral; ?>
                 <?php echo $controlador->inputs->select->cat_sat_tipo_nomina_id; ?>
                 <?php echo $controlador->inputs->select->cat_sat_periodicidad_pago_nom_id; ?>
-                <?php echo $controlador->inputs->num_dias_pagados; ?>
-                <?php echo $controlador->inputs->salario_diario; ?>
-                <?php echo $controlador->inputs->salario_diario_integrado; ?>
                 <?php echo $controlador->inputs->fecha_pago; ?>
                 <?php echo $controlador->inputs->fecha_inicial_pago; ?>
                 <?php echo $controlador->inputs->fecha_final_pago; ?>
+                <?php echo $controlador->inputs->num_dias_pagados; ?>
+                <?php echo $controlador->inputs->salario_diario; ?>
+                <?php echo $controlador->inputs->salario_diario_integrado; ?>
+                <?php echo $controlador->inputs->subtotal; ?>
 
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
