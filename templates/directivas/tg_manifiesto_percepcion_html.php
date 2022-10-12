01b0581be0157feb4f816b223c7aece335734249
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-12 11:56:00 -0500

    v0.111.10 Se ajustan campos en alta manifiesto percepcion

--- templates/directivas/tg_manifiesto_percepcion_html.php
+++ templates/directivas/tg_manifiesto_percepcion_html.php
@@ -6,20 +6,20 @@ use gamboamartin\errores\errores;
 use gamboamartin\template\directivas;
 use PDO;
 use stdClass;
-use tglobally\tg_nomina\controllers\controlador_tg_row_manifiesto;
+use tglobally\tg_nomina\controllers\controlador_tg_manifiesto_percepcion;
 
 class tg_manifiesto_percepcion_html extends em_html {
 
-    private function asigna_inputs(controlador_tg_row_manifiesto $controler, array $inputs): array|stdClass
+    private function asigna_inputs(controlador_tg_manifiesto_percepcion $controler, array $inputs): array|stdClass
     {
         $controler->inputs->select = new stdClass();
-        $controler->inputs->select->tg_manifiesto_percepcion = $inputs['selects']->tg_manifiesto_percepcion;
+        $controler->inputs->select->tg_row_manifiesto_id = $inputs['selects']->tg_row_manifiesto_id;
         $controler->inputs->select->nom_par_percepcion_id = $inputs['selects']->nom_par_percepcion_id;
 
         return $controler->inputs;
     }
 
-    public function genera_inputs(controlador_tg_row_manifiesto $controler, array $keys_selects = array()): array|stdClass
+    public function genera_inputs(controlador_tg_manifiesto_percepcion $controler, array $keys_selects = array()): array|stdClass
     {
         $inputs = $this->init_alta2(row_upd: $controler->row_upd, modelo: $controler->modelo, link: $controler->link,
             keys_selects:$keys_selects);
---
+++ views/tg_manifiesto_percepcion/modifica.php
@@ -0,0 +1,31 @@
+<?php /** @var \tglobally\tg_nomina\controllers\controlador_tg_tipo_servicio $controlador */ ?>
+<?php include 'templates/tg_tipo_servicio/modifica/secciones.php'; ?>
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+
+        <div class="  form-main" id="form">
+            <form method="post" action="./index.php?seccion=tg_row_manifiesto&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+
+                <?php echo $controlador->inputs->id; ?>
+                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->codigo_bis; ?>
+                <?php echo $controlador->inputs->descripcion; ?>
+                <?php echo $controlador->inputs->descripcion_select; ?>
+                <?php echo $controlador->inputs->alias; ?>
+                <?php echo $controlador->inputs->select->tg_manifiesto_id; ?>
+                <?php echo $controlador->inputs->select->em_empleado_id; ?>
+
+                <div class="buttons col-md-12">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " value="modifica">Guarda</button>
+                    </div>
+                    <div class="col-md-6 ">
+                        <a href="index.php?seccion=tg_row_manifiesto&accion=lista&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Lista</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
