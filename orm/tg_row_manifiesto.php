08a9278a237781d43142e9c9297dca26166227d3
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-12 10:55:45 -0500

    v0.108.10 Se ajustan campos en alta row manifiesto

--- orm/tg_row_manifiesto.php
+++ orm/tg_row_manifiesto.php
@@ -2,6 +2,7 @@
 namespace models;
 use base\orm\modelo;
 
+use gamboamartin\empleado\models\em_empleado;
 use gamboamartin\errores\errores;
 
 use PDO;
@@ -14,8 +15,13 @@ class tg_row_manifiesto extends modelo{
         $columnas = array($tabla=>false);
         $campos_obligatorios = array('descripcion','codigo','descripcion_select','alias','codigo_bis');
 
+        $campos_view['tg_manifiesto_id']['type'] = 'selects';
+        $campos_view['tg_manifiesto_id']['model'] = (new tg_manifiesto($link));
+        $campos_view['em_empleado_id']['type'] = 'selects';
+        $campos_view['em_empleado_id']['model'] = (new em_empleado($link));
+
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, campos_view: $campos_view);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
--- templates/directivas/tg_row_manifiesto_html.php
+++ templates/directivas/tg_row_manifiesto_html.php
@@ -4,8 +4,34 @@ namespace html;
 use gamboamartin\errores\errores;
 use gamboamartin\template\directivas;
 use PDO;
+use stdClass;
+use tglobally\tg_nomina\controllers\controlador_tg_row_manifiesto;
 
 class tg_row_manifiesto_html extends em_html {
 
+    private function asigna_inputs(controlador_tg_row_manifiesto $controler, array $inputs): array|stdClass
+    {
+        $controler->inputs->select = new stdClass();
+        $controler->inputs->select->tg_manifiesto_id = $inputs['selects']->tg_manifiesto_id;
+        $controler->inputs->select->em_empleado_id = $inputs['selects']->em_empleado_id;
+
+        return $controler->inputs;
+    }
+
+    public function genera_inputs(controlador_tg_row_manifiesto $controler, array $keys_selects = array()): array|stdClass
+    {
+        $inputs = $this->init_alta2(row_upd: $controler->row_upd, modelo: $controler->modelo, link: $controler->link,
+            keys_selects:$keys_selects);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+        }
+
+        $inputs_asignados = $this->asigna_inputs(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
 
 }
