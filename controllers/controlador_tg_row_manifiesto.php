e0091ecbe47f5d344a3850b4a9637e23e9ee9be6
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-11-02 12:27:55 -0600

    v0.134.10 Se ajusta constructos de controlers

--- controllers/controlador_tg_row_manifiesto.php
+++ controllers/controlador_tg_row_manifiesto.php
@@ -18,7 +18,7 @@ class controlador_tg_row_manifiesto extends system {
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new tg_row_manifiesto(link: $link);
         $html_ = new tg_row_manifiesto_html(html: $html);
-        $obj_link = new links_menu($this->registro_id);
+        $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
         parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Row Manifiesto';
