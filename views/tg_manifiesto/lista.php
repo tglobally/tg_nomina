9c19b83e86c7556e5ae1833a07182366fff94a20
[user] <angel.martinez@ivitec.com.mx>
2023-01-23 16:36:40 -0600

    v0.170.12 Se ajusta lista datatable tg manifiesto

--- views/tg_manifiesto/lista.php
+++ views/tg_manifiesto/lista.php
@@ -1,4 +1,4 @@
-<?php /** @var \tglobally\tg_nomina\controllers\controlador_tg_manifiesto $controlador */ ?>
+<?php /** @var \tglobally\tg_empleado\controllers\controlador_em_empleado  $controlador */ ?>
 <?php
 
 use config\views;
@@ -16,35 +16,7 @@ $url_icons = (new views())->url_icons;
 
                 <div class="card-body">
                     <div class="cont_tabla_sucursal  col-md-12">
-                        <table class="table ">
-                            <thead>
-                            <tr>
-                                <th data-breakpoints="xs sm md" data-type="html" >Id</th>
-                                <th data-breakpoints="xs sm md" data-type="html" >Codigo</th>
-                                <th data-breakpoints="xs sm md" data-type="html" >Codigo bis</th>
-                                <th data-breakpoints="xs sm md" data-type="html" >Descripcion</th>
-                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Sube Manifiesto</th>
-                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Ve Nominas</th>
-                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Modifica</th>
-                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Elimina</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php foreach ($controlador->registros as $registro){?>
-                                <tr>
-                                    <td><?php echo $registro->tg_manifiesto_id; ?></td>
-                                    <td><?php echo $registro->tg_manifiesto_codigo; ?></td>
-                                    <td><?php echo $registro->tg_manifiesto_codigo_bis?></td>
-                                    <td><?php echo $registro->tg_manifiesto_descripcion; ?></td>
-                                    <td><a class="btn btn-info " href="<?php echo $registro->link_sube_manifiesto; ?>">Sube Manifiesto</a></td>
-                                    <td><a class="btn btn-info " href="<?php echo $registro->link_ve_nominas; ?>">Ve Nominas</a></td>
-                                    <td><a class="btn btn-info " href="<?php echo $registro->link_descarga_nomina; ?>">Descarga Nomina</a></td>
-                                    <td><a class="btn btn-warning " href="<?php echo $registro->link_modifica; ?>">Modifica</a></td>
-                                    <td><a class="btn btn-danger " href="<?php echo $registro->link_elimina_bd; ?>">Elimina</a></td>
-                                </tr>
-                            <?php } ?>
-
-                            </tbody>
+                        <table class="table table-striped datatable">
                         </table>
                     </div>
                 </div>
