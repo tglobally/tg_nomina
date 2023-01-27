7115732111fa95fd13d7e40401e767b1fb3d6c54
Kevin <kevin.acuna@ivitec.com.mx>
2023-01-27 14:48:51 -0500

    v0.175.12 Se integra view para reportes de nominas por periodos

--- controllers/controlador_nom_periodo.php
+++ controllers/controlador_nom_periodo.php
@@ -3,6 +3,7 @@ namespace tglobally\tg_nomina\controllers;
 
 use gamboamartin\errores\errores;
 use gamboamartin\nomina\models\nom_nomina;
+use gamboamartin\nomina\models\nom_periodo;
 use PDO;
 use stdClass;
 use tglobally\template_tg\html;
@@ -10,15 +11,33 @@ use tglobally\template_tg\html;
 class controlador_nom_periodo extends \gamboamartin\nomina\controllers\controlador_nom_periodo {
 
     public array $sidebar = array();
+    public array $keys_selects = array();
+    public string $link_nom_periodo_reportes = '';
 
     public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
         $html_base = new html();
         parent::__construct( link: $link, html: $html_base);
         $this->titulo_lista = 'Periodos';
 
+        $campos_view['filtro_fecha_inicio'] = array('type' => 'dates');
+        $campos_view['filtro_fecha_final'] = array('type' => 'dates');
+
+        $this->modelo->campos_view = $campos_view;
+
+        $this->link_nom_periodo_reportes = $this->obj_link->link_con_id(accion: "reportes", link: $link, registro_id: $this->registro_id,
+            seccion: $this->seccion);
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al obtener link',
+                data: $this->link_nom_periodo_reportes);
+            print_r($error);
+            exit;
+        }
+
         $this->sidebar['lista']['titulo'] = "Periodos";
         $this->sidebar['lista']['menu'] = array(
             $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta, menu_seccion_active: true,
+                menu_lateral_active: true),
+            $this->menu_item(menu_item_titulo: "Reportes", link: $this->link_nom_periodo_reportes, menu_seccion_active: true,
                 menu_lateral_active: true));
 
         $this->sidebar['alta']['titulo'] = "Alta Periodos";
@@ -56,8 +75,48 @@ class controlador_nom_periodo extends \gamboamartin\nomina\controllers\controlad
 
     public function reportes(bool $header, bool $ws = false): array|stdClass
     {
+        $columns["nom_nomina_id"]["titulo"] = "Id";
+        $columns["em_empleado_rfc"]["titulo"] = "Rfc";
+        $columns["em_empleado_nombre"]["titulo"] = "Empleado";
+        $columns["em_empleado_nombre"]["campos"] = array("em_empleado_ap","em_empleado_am");
 
 
-        return array();
+        $filtro = array();
+
+        $datatable = $this->datatable_init(columns: $columns,identificador: "#nom_nomina");
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al inicializar datatable',data:  $datatable,
+                header: $header,ws:$ws);
+        }
+
+        $this->asignar_propiedad(identificador: 'filtro_fecha_inicio', propiedades: ['place_holder'=> 'Fecha Inicio',
+            'cols' => 6, 'required' => false]);
+        $this->asignar_propiedad(identificador: 'filtro_fecha_final', propiedades: ['place_holder'=> 'Fecha Final',
+            'cols' => 6]);
+
+        $r_alta =  parent::alta(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $this->inputs;
+    }
+
+    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    {
+        if (!array_key_exists($identificador,$this->keys_selects)){
+            $this->keys_selects[$identificador] = new stdClass();
+        }
+
+        foreach ($propiedades as $key => $value){
+            $this->keys_selects[$identificador]->$key = $value;
+        }
     }
 }
--- views/nom_periodo/reportes.php
+++ views/nom_periodo/reportes.php
@@ -1,3 +1,33 @@
 <?php /** @var \tglobally\tg_nomina\controllers\controlador_nom_periodo $controlador */ ?>
 
 <?php (new \tglobally\template_tg\template())->sidebar($controlador); ?>
+
+<div class="col-md-9 info-lista">
+    <div class="col-lg-12 content">
+        <h3 class="text-center titulo-form">
+            Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h3>
+
+        <div class="lista">
+            <div class="card">
+
+                <div class="card-body">
+                    <div class="cont_tabla_sucursal  col-md-12">
+                        <div class="botones" style="display: flex; justify-content: flex-end; align-items: center">
+                            <form method="post" action="<?php echo $controlador->link_em_empleado_exportar; ?> "
+                                  class="form-additional" id="form_export" style="width: 100%">
+                                <?php echo $controlador->inputs->filtro_fecha_inicio; ?>
+                                <?php echo $controlador->inputs->filtro_fecha_final; ?>
+                            </form>
+                            <button type="submit" class="btn btn-info" name="btn_action_next"
+                                    style="border-radius: 5px" value="exportar" form="form_export">
+                                Exportar
+                            </button>
+                        </div>
+                        <table id="nom_nomina" class="table table-striped "></table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
