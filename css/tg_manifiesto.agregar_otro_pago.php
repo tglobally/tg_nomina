9e44b97bebfe21e2d98d2085f8cb09c4a4a62b6d
Kevin <kevin.acuna@ivitec.com.mx>
2023-02-14 16:56:12 -0500

    v0.199.13 Se anexan estilos

--- css/tg_manifiesto.agregar_otro_pago.php
+++ css/tg_manifiesto.agregar_otro_pago.php
@@ -6,13 +6,9 @@ $ruta_template_base = (new views())->ruta_template_base;
 include $ruta_template_base.'assets/css/_base_css.php';
 ?>
 <style>
-
-    .card-header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+    body .pagination li.page-item a:hover, body .pagination li.page-item a.active, body .pagination-carousel li a:hover, body .pagination-carousel li.active a, .header .top-bar .pull-right, body .color-secondary, body .btn.color-secondary {
+        background-color: white;
     }
-
 
     tr.group,
     tr.group:hover {
