11379b70f0ce7145a32cee61657c41c89672eeb5
Javier Flores <javier.flores@ivitec.com.mx>
2023-03-01 16:43:25 -0600

    v0.323.28 se ajusta controlador tg agrupador

--- controllers/controlador_tg_agrupador.php
+++ controllers/controlador_tg_agrupador.php
@@ -1,46 +1,96 @@
 <?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
 namespace tglobally\tg_nomina\controllers;
-
 use gamboamartin\errores\errores;
 use gamboamartin\system\_ctl_base;
 use gamboamartin\system\links_menu;
-use gamboamartin\system\system;
+
 use gamboamartin\template\html;
+use html\bn_cuenta_html;
 use html\tg_agrupador_html;
-use tglobally\tg_nomina\models\tg_agrupador;
 use PDO;
 use stdClass;
+use tglobally\tg_nomina\models\tg_agrupador;
 
 class controlador_tg_agrupador extends _ctl_base {
+    public array $sidebar = array();
 
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass()){
+
         $modelo = new tg_agrupador(link: $link);
         $html_ = new tg_agrupador_html(html: $html);
-        $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
-        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+        $obj_link = new links_menu(link: $link, registro_id:$this->registro_id);
 
-        $this->titulo_lista = 'Agrupador';
+
+        $datatables = new stdClass();
+        $datatables->columns = array();
+        $datatables->columns['tg_agrupador_id']['titulo'] = 'Id';
+        $datatables->columns['tg_agrupador_codigo']['titulo'] = 'Cod';
+        $datatables->columns['tg_agrupador_descripcion']['titulo'] = 'Observaciones';
+
+        $datatables->filtro = array();
+        $datatables->filtro[] = 'tg_agrupador.id';
+        $datatables->filtro[] = 'tg_agrupador.codigo';
+        $datatables->filtro[] = 'tg_agrupador.descripcion';
+
+
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link,
+            datatables: $datatables, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Clasificacion';
+
+        $this->lista_get_data = true;
+
+        $this->sidebar['lista']['titulo'] = "Agrupador";
+        $this->sidebar['lista']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta, menu_seccion_active: true,
+                menu_lateral_active: true));
+
+        $this->sidebar['alta']['titulo'] = "Alta Agrupador";
+        $this->sidebar['alta']['stepper_active'] = true;
+        $this->sidebar['alta']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta, menu_lateral_active: true));
+
+        $this->sidebar['modifica']['titulo'] = "Modifica Agrupador";
+        $this->sidebar['modifica']['stepper_active'] = true;
+        $this->sidebar['modifica']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_modifica, menu_lateral_active: true));
     }
 
     public function alta(bool $header, bool $ws = false): array|string
     {
+
         $r_alta = $this->init_alta();
-        if (errores::$error) {
-            return $this->retorno_error(mensaje: 'Error al inicializar alta', data: $r_alta, header: $header, ws: $ws);
+        if(errores::$error){
+            return $this->retorno_error(
+                mensaje: 'Error al inicializar alta',data:  $r_alta, header: $header,ws:  $ws);
         }
 
-        $keys_selects = $this->init_selects_inputs();
-        if (errores::$error) {
-            return $this->retorno_error(mensaje: 'Error al inicializar selects', data: $keys_selects, header: $header,
-                ws: $ws);
-        }
+        $keys_selects['descripcion'] = new stdClass();
+        $keys_selects['descripcion']->cols = 6;
+
+        $keys_selects['fecha de pago'] = new stdClass();
+        $keys_selects['fecha de pago']->cols = 12;
+
+        $keys_selects['Monto'] = new stdClass();
+        $keys_selects['Monto']->cols = 6;
+
+
 
         $inputs = $this->inputs(keys_selects: $keys_selects);
-        if (errores::$error) {
+        if(errores::$error){
             return $this->retorno_error(
-                mensaje: 'Error al obtener inputs', data: $inputs, header: $header, ws: $ws);
+                mensaje: 'Error al obtener inputs',data:  $inputs, header: $header,ws:  $ws);
         }
+
+
 
         return $r_alta;
     }
@@ -48,47 +98,90 @@ class controlador_tg_agrupador extends _ctl_base {
     protected function campos_view(): array
     {
         $keys = new stdClass();
-        $keys->inputs = array('codigo', 'codigo_bis', 'descripcion', 'descripcion_select', 'alias');
+        $keys->inputs = array('id','codigo','descripcion');
         $keys->selects = array();
+        $keys->fechas = array();
 
         $init_data = array();
-        $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
-        if (errores::$error) {
-            return $this->errores->error(mensaje: 'Error al inicializar campo view', data: $campos_view);
+        $campos_view = $this->campos_view_base(init_data: $init_data,keys:  $keys);
+
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al inicializar campo view',data:  $campos_view);
         }
+
 
         return $campos_view;
     }
 
-    private function init_selects(array $keys_selects, string $key, string $label, int $id_selected = -1, int $cols = 6,
-                                  bool  $con_registros = true, array $filtro = array()): array
+    protected function inputs_children(stdClass $registro): stdClass|array
     {
-        $keys_selects = $this->key_select(cols: $cols, con_registros: $con_registros, filtro: $filtro, key: $key,
-            keys_selects: $keys_selects, id_selected: $id_selected, label: $label);
+        $this->inputs = new stdClass();
+        $this->inputs->select = new stdClass();
+        return $this->inputs;
+    }
+
+
+    protected function key_selects_txt(array $keys_selects): array
+    {
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 12,key: 'id', keys_selects:$keys_selects, place_holder: 'Id');
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'codigo', keys_selects: $keys_selects, place_holder: 'Cod');
         if (errores::$error) {
             return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6,key: 'descripcion', keys_selects:$keys_selects, place_holder: 'Observaciones');
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects',data:  $keys_selects);
         }
 
         return $keys_selects;
     }
 
-
-
-
-    public function modifica(bool $header, bool $ws = false): array|stdClass
+    public function modifica(
+        bool $header, bool $ws = false): array|stdClass
     {
-        $r_modifica = $this->init_modifica();
-        if (errores::$error) {
+        $r_modifica = $this->init_modifica(); // TODO: Change the autogenerated stub
+        if(errores::$error){
             return $this->retorno_error(
-                mensaje: 'Error al generar salida de template', data: $r_modifica, header: $header, ws: $ws);
+                mensaje: 'Error al generar salida de template',data:  $r_modifica,header: $header,ws: $ws);
         }
 
-        $base = $this->base_upd(keys_selects: array(), params: array(), params_ajustados: array());
-        if (errores::$error) {
-            return $this->retorno_error(mensaje: 'Error al integrar base', data: $base, header: $header, ws: $ws);
+        $keys_selects['descripcion'] = new stdClass();
+        $keys_selects['descripcion']->cols = 6;
+
+        $keys_selects['codigo'] = new stdClass();
+        $keys_selects['codigo']->disabled = true;
+
+        $keys_selects['Fecha de Pago'] = new stdClass();
+        $keys_selects['Fecha de Pago']->cols = 6;
+
+        $keys_selects['Monto'] = new stdClass();
+        $keys_selects['Monto']->cols = 6;
+
+        $base = $this->base_upd(keys_selects: $keys_selects, params: array(),params_ajustados: array());
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al integrar base',data:  $base, header: $header,ws:  $ws);
         }
 
         return $r_modifica;
     }
+
+    public function menu_item(string $menu_item_titulo, string $link, bool $menu_seccion_active = false, bool $menu_lateral_active = false): array
+    {
+        $menu_item = array();
+        $menu_item['menu_item'] = $menu_item_titulo;
+        $menu_item['menu_seccion_active'] = $menu_seccion_active;
+        $menu_item['link'] = $link;
+        $menu_item['menu_lateral_active'] = $menu_lateral_active;
+
+        return $menu_item;
+    }
+
+
 
 }
