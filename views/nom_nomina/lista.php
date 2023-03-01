a10cc8e92bdb8f5ea67f21fe0f4ed751020aa95c
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-02-28 18:37:26 -0600

    v0.328.18 Se ajusta lsita nomina

--- views/nom_nomina/lista.php
+++ views/nom_nomina/lista.php
@@ -2,7 +2,7 @@
 
 <?php (new \tglobally\template_tg\template())->sidebar($controlador); ?>
 
-<div class="col-md-10 info-lista">
+<div class="col-md-9 info-lista">
     <div class="col-lg-12 content">
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h3>
 
@@ -20,3 +20,5 @@
 
     </div>
 </div>
+
+
