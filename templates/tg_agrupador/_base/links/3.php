0b48a289752c87bf5e698c0c262a1e4488f6095b
Javier Flores <javier.flores@ivitec.com.mx>
2023-03-03 17:50:51 -0600

    v0.323.34 ajuste de redireccion

--- templates/tg_agrupador/_base/links/3.php
+++
@@ -1,4 +0,0 @@
-<?php /** @var controllers\controlador_org_empresa $controlador */ ?>
-<a href="index.php?seccion=org_empresa&accion=cif&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>">
-    <?php include "templates/org_empresa/_base/buttons/3.gris.php"; ?>
-</a>
