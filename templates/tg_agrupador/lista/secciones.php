f37663839a1db813c88b49280847bf424399fb52
Javier Flores <javier.flores@ivitec.com.mx>
2023-03-07 12:19:14 -0600

    v0.323.42 ahora los registros se dan de alta correctamente

--- templates/tg_agrupador/lista/secciones.php
+++ templates/tg_agrupador/lista/secciones.php
@@ -1,7 +1,7 @@
 <div class="col-md-3 secciones">
     <div class="col-md-12 int_secciones ">
         <div class="col-md-8">
-            <h3>Layout</h3>
+            <h3>Agrupador</h3>
             <?php include "templates/tg_agrupador/_base/links/alta.php"; ?>
             <hr class="hr-menu-lateral">
         </div>
--- templates/tg_agrupador/modifica/secciones.php
+++ templates/tg_agrupador/modifica/secciones.php
@@ -6,7 +6,7 @@
             <img src="<?php echo (new views())->url_assets.'img/stepper/1.svg'?>" class="img-seccion">
         </div>
         <div class="col-md-8">
-            <h3>Modifica Layout</h3>
+            <h3>Modifica Agrupador</h3>
             <?php include "templates/tg_agrupador/_base/buttons/1.azul.modifica.php"; ?>
         </div>
     </div>
