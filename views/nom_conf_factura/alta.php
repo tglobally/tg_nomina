659949e49227ea043e443eee7f3ed4243f3f28c1
[user] <angel.martinez@ivitec.com.mx>
2023-03-13 15:22:52 -0600

    v0.334.46 Se ajustan views conf factura

--- views/nom_conf_factura/alta.php
+++ views/nom_conf_factura/alta.php
@@ -10,14 +10,19 @@
             <form method="post" action="./index.php?seccion=nom_conf_factura&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
                 <?php echo $controlador->inputs->cat_sat_forma_pago_id; ?>
                 <?php echo $controlador->inputs->cat_sat_metodo_pago_id; ?>
-                <?php echo $controlador->inputs->cat_sat_moneda_id; ?>
                 <?php echo $controlador->inputs->com_tipo_cambio_id; ?>
-                <?php echo $controlador->inputs->cat_sat_uso_cfdi_id; ?>
                 <?php echo $controlador->inputs->cat_sat_tipo_de_comprobante_id; ?>
-                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->cat_sat_moneda_id; ?>
+                <?php echo $controlador->inputs->cat_sat_uso_cfdi_id; ?>
+                <?php echo $controlador->inputs->com_producto_id; ?>
                 <?php echo $controlador->inputs->descripcion; ?>
                 <div class="buttons col-md-12">
-                    <?php echo $controlador->btns['sub_guarda']; ?>
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="index.php?seccion=tg_manifiesto&accion=lista&session_id=<?php echo $controlador->session_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Regresar</a>
+                    </div>
                 </div>
             </form>
         </div>
--- views/nom_conf_factura/modifica.php
+++ views/nom_conf_factura/modifica.php
@@ -10,11 +10,11 @@
             <form method="post" action="./index.php?seccion=nom_conf_factura&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
                 <?php echo $controlador->inputs->cat_sat_forma_pago_id; ?>
                 <?php echo $controlador->inputs->cat_sat_metodo_pago_id; ?>
-                <?php echo $controlador->inputs->cat_sat_moneda_id; ?>
                 <?php echo $controlador->inputs->com_tipo_cambio_id; ?>
-                <?php echo $controlador->inputs->cat_sat_uso_cfdi_id; ?>
                 <?php echo $controlador->inputs->cat_sat_tipo_de_comprobante_id; ?>
-                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->cat_sat_moneda_id; ?>
+                <?php echo $controlador->inputs->cat_sat_uso_cfdi_id; ?>
+                <?php echo $controlador->inputs->com_producto_id; ?>
                 <?php echo $controlador->inputs->descripcion; ?>
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
