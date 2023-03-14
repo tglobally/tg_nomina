983aa844479f90d7d79c51d58f298c23987ee89a
[user] <angel.martinez@ivitec.com.mx>
2023-03-14 14:54:58 -0600

    v0.338.47 Se integra nom clasificacion

--- controllers/controlador_nom_clasificacion.php
+++ controllers/controlador_nom_clasificacion.php
@@ -8,9 +8,38 @@ use tglobally\template_tg\html;
 
 class controlador_nom_clasificacion extends \gamboamartin\nomina\controllers\controlador_nom_clasificacion {
 
+    public array $sidebar = array();
+
     public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
         $html_base = new html();
         parent::__construct( link: $link, html: $html_base);
-        $this->titulo_lista = 'Nominas';
+        $this->titulo_lista = 'Clasificación Nóminas';
+
+        $this->sidebar['lista']['titulo'] = "Clasificacion Nominas";
+        $this->sidebar['lista']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_seccion_active: true,
+                menu_lateral_active: true));
+
+        $this->sidebar['alta']['titulo'] = "Alta Clasificacion Nominas";
+        $this->sidebar['alta']['stepper_active'] = true;
+        $this->sidebar['alta']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_lateral_active: true));
+
+        $this->sidebar['modifica']['titulo'] = "Modifica Clasificacion Nominas";
+        $this->sidebar['modifica']['stepper_active'] = true;
+        $this->sidebar['modifica']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_alta,menu_lateral_active: true));
+
+    }
+
+    public function menu_item(string $menu_item_titulo, string $link, bool $menu_seccion_active = false,bool $menu_lateral_active = false): array
+    {
+        $menu_item = array();
+        $menu_item['menu_item'] = $menu_item_titulo;
+        $menu_item['menu_seccion_active'] = $menu_seccion_active;
+        $menu_item['link'] = $link;
+        $menu_item['menu_lateral_active'] = $menu_lateral_active;
+
+        return $menu_item;
     }
 }
--- templates/tg_column/lista/secciones.php
+++ templates/tg_column/lista/secciones.php
@@ -3,7 +3,6 @@
         <div class="col-md-8">
             <h3>Column</h3>
             <?php include "templates/tg_column/_base/links/alta.php"; ?>
-            <hr class="hr-menu-lateral">
         </div>
     </div>
 </div>
