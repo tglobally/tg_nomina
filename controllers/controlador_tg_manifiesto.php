2e1d74f000db7563ad87aeab09017909da2ce639
Kevin <kevin.acuna@ivitec.com.mx>
2023-03-21 16:24:39 -0500

    v0.345.50 Se ajusta salida

--- controllers/controlador_tg_manifiesto.php
+++ controllers/controlador_tg_manifiesto.php
@@ -516,6 +516,7 @@ class controlador_tg_manifiesto extends _ctl_base
         $datatables = new stdClass();
         $datatables->columns = $columns;
         $datatables->filtro = $filtro;
+        $datatables->menu_active = true;
 
         return $datatables;
     }
