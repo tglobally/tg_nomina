81d57557bfc59ac0537dac7f6dcf81695cc1c7cb
Javier Flores <javier.flores@ivitec.com.mx>
2023-03-23 16:06:54 -0600

    v0.350.51 se integra sube manifiesto a menu lateral de lista

--- templates/tg_manifiesto/lista/secciones.php
+++ templates/tg_manifiesto/lista/secciones.php
@@ -3,6 +3,7 @@
         <div class="col-md-8">
             <h3>Manifiesto</h3>
             <?php include "templates/tg_manifiesto/_base/links/alta.php"; ?>
+            <?php include "templates/tg_manifiesto/_base/links/sube_manifiesto.php"; ?>
         </div>
     </div>
 </div>
