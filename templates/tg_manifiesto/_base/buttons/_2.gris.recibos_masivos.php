95f81f839b9410510389bbae525074d697799138
CarlosFigueroa18 <carlos.figueroa@ivitec.com.mx>
2023-04-04 15:14:31 -0600

    v0.352.58 FIX btn 3 recibos masivos en manifiesto

--- templates/tg_manifiesto/_base/buttons/_2.gris.recibos_masivos.php
+++ templates/tg_manifiesto/_base/buttons/_2.gris.recibos_masivos.php
@@ -1,2 +1,2 @@
-<?php include "templates/tg_manifiesto/_base/numero/2.gris.php"; ?>
+<?php include "templates/tg_manifiesto/_base/numero/3.gris.php"; ?>
 <?php include "templates/tg_manifiesto/_base/texto_menu_lateral/recibos_masivos.php"; ?>
