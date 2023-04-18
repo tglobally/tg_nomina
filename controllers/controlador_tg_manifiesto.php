f0f2b49d2b733331b4c3c55a7c1a6650a715ba2b
Kevin <kevin.acuna@ivitec.com.mx>
2023-04-18 11:24:22 -0500

    v0.370.66 Se integra salida para despensa

--- controllers/controlador_tg_manifiesto.php
+++ controllers/controlador_tg_manifiesto.php
@@ -890,11 +890,19 @@ class controlador_tg_manifiesto extends _ctl_base
                     header: $header,ws:$ws);
             }
 
+            $despensa = (new nom_par_percepcion($this->link))->filtro_and(filtro: array("nom_nomina_id" => $nomina['nom_nomina_id'],
+                "nom_percepcion.descripcion" => 'Despensa'));
+            if(errores::$error){
+                return $this->retorno_error(mensaje: 'Error al obtener prima dominical de la nomina',data:  $despensa,
+                    header: $header,ws:$ws);
+            }
+
             $total_subsidio = 0;
             $total_prima_dominical = 0;
             $total_vacaciones = 0;
             $total_septimo_dia = 0;
             $total_compensacion = 0;
+            $total_despensa = 0;
 
             foreach ($subsidios->registros as $subsidio){
                 $total_subsidio += $subsidio['nom_par_otro_pago_importe_gravado'] + $subsidio['nom_par_otro_pago_importe_exento'];
@@ -914,6 +922,10 @@ class controlador_tg_manifiesto extends _ctl_base
 
             foreach ($compensacion->registros as $registro){
                 $total_compensacion += $registro['nom_par_percepcion_importe_gravado'] + $registro['nom_par_percepcion_importe_exento'];
+            }
+
+            foreach ($despensa->registros as $registro){
+                $total_despensa += $registro['nom_par_percepcion_importe_gravado'] + $registro['nom_par_percepcion_importe_exento'];
             }
 
             $uuid = "";
@@ -949,7 +961,8 @@ class controlador_tg_manifiesto extends _ctl_base
                 $total_prima_dominical,
                 $total_vacaciones,
                 $total_septimo_dia,
-                $total_compensacion
+                $total_compensacion,
+                $total_despensa
             ];
             $registros[] = $registro;
         }
