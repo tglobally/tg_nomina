1a28b8c38e65381e7f7be01769b266340f9662cb
Kevin <kevin.acuna@ivitec.com.mx>
2023-06-29 13:28:38 -0500

    v0.443.73 Se sobrescribe modelo

--- controllers/controlador_em_empleado.php
+++ controllers/controlador_em_empleado.php
@@ -4,12 +4,15 @@ namespace tglobally\tg_nomina\controllers;
 use gamboamartin\errores\errores;
 use PDO;
 use stdClass;
+use tglobally\tg_nomina\models\em_empleado;
 
 class controlador_em_empleado extends \tglobally\tg_empleado\controllers\controlador_em_empleado {
 
     public function __construct(PDO $link, stdClass $paths_conf = new stdClass())
     {
         parent::__construct($link, $paths_conf);
+
+        $this->modelo = new em_empleado($link);
     }
 
     protected function campos_view(): array
