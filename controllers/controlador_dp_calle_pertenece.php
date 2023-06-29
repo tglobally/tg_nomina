7d6fb783c307c6e41af4df28093441614338ecf7
Kevin <kevin.acuna@ivitec.com.mx>
2023-06-29 13:23:36 -0500

    v0.442.73 Se anexan controladores dependientes

---
+++ controllers/controlador_dp_calle_pertenece.php
@@ -0,0 +1,3 @@
+<?php
+namespace tglobally\tg_nomina\controllers;
+class controlador_dp_calle_pertenece extends \tglobally\tg_empleado\controllers\controlador_dp_calle_pertenece {}
---
+++ controllers/controlador_dp_colonia_postal.php
@@ -0,0 +1,4 @@
+<?php
+namespace tglobally\tg_nomina\controllers;
+
+class controlador_dp_colonia_postal extends \tglobally\tg_empleado\controllers\controlador_dp_colonia_postal {}
