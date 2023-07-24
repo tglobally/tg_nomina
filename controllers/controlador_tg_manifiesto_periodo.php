44849a834c3c5bba3a64152ce65d303dd08af503
Kevin <kevin.acuna@ivitec.com.mx>
2023-07-24 12:37:58 -0500

    v0.457.74 Se ajusta vistas en periodos manifiesto

--- controllers/controlador_tg_manifiesto_periodo.php
+++ controllers/controlador_tg_manifiesto_periodo.php
@@ -19,7 +19,6 @@ use stdClass;
 
 class controlador_tg_manifiesto_periodo extends system
 {
-
     public array|stdClass $keys_selects = array();
 
     public function __construct(PDO      $link, html $html = new \gamboamartin\template_1\html(),
@@ -28,12 +27,30 @@ class controlador_tg_manifiesto_periodo extends system
         $modelo = new tg_manifiesto_periodo(link: $link);
         $html_ = new tg_manifiesto_periodo_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
-        parent::__construct(html: $html_, link: $link, modelo: $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
-        $this->titulo_lista = 'Manifiesto Periodo';
+        $datatables = $this->init_datatable();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar datatable', data: $datatables);
+            print_r($error);
+            die('Error');
+        }
+
+        parent::__construct(html: $html_, link: $link, modelo: $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
+
+        $this->seccion_titulo = 'Periodos Manifiestos';
+        $this->titulo_pagina = "Nominas - Periodos Manifiestos";
+        $this->titulo_accion = "Periodos Manifiestos";
 
         $this->asignar_propiedad(identificador:'tg_manifiesto_id', propiedades: ["label" => "Manifiesto"]);
         $this->asignar_propiedad(identificador:'nom_periodo_id', propiedades: ["label" => "Periodo Nomina"]);
+
+        $acciones = $this->define_acciones_menu(acciones: array("alta" => $this->link_alta));
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al integrar acciones para el menu', data: $acciones);
+            print_r($error);
+            die('Error');
+        }
     }
 
     public function alta(bool $header, bool $ws = false): array|string
@@ -50,6 +67,24 @@ class controlador_tg_manifiesto_periodo extends system
             die('Error');
         }
         return $r_alta;
+    }
+
+    private function init_datatable(): stdClass
+    {
+        $columns["tg_manifiesto_periodo_id"]["titulo"] = "Id";
+        $columns["tg_manifiesto_descripcion"]["titulo"] = "Manifiesto";
+        $columns["nom_periodo_fecha_inicial_pago"]["titulo"] = "Fecha Inicial";
+        $columns["nom_periodo_fecha_final_pago"]["titulo"] = "Fecha Final";
+
+        $filtro = array("tg_manifiesto_periodo.id", "tg_manifiesto.descripcion", "nom_periodo.fecha_inicial_pago",
+            "nom_periodo.fecha_final_pago");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+        $datatables->menu_active = true;
+
+        return $datatables;
     }
 
     public function asignar_propiedad(string $identificador, array $propiedades): array|stdClass
--- views/tg_manifiesto_periodo/modifica.php
+++ views/tg_manifiesto_periodo/modifica.php
@@ -1,31 +1,17 @@
 <?php /** @var \tglobally\tg_nomina\controllers\controlador_tg_manifiesto_periodo $controlador */ ?>
-<?php include 'templates/tg_manifiesto_periodo/modifica/secciones.php'; ?>
-<div class="col-md-9 formulario">
-    <div class="col-lg-12">
+<form class="row g-3" method="post" action="<?php echo $controlador->link_modifica_bd; ?>">
 
-        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+    <?php echo $controlador->inputs->id; ?>
+    <?php echo $controlador->inputs->codigo; ?>
+    <?php echo $controlador->inputs->codigo_bis; ?>
+    <?php echo $controlador->inputs->descripcion; ?>
+    <?php echo $controlador->inputs->descripcion_select; ?>
+    <?php echo $controlador->inputs->alias; ?>
+    <?php echo $controlador->inputs->tg_manifiesto_id; ?>
+    <?php echo $controlador->inputs->nom_periodo_id; ?>
 
-        <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=tg_manifiesto_periodo&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
-
-                <?php echo $controlador->inputs->id; ?>
-                <?php echo $controlador->inputs->codigo; ?>
-                <?php echo $controlador->inputs->codigo_bis; ?>
-                <?php echo $controlador->inputs->descripcion; ?>
-                <?php echo $controlador->inputs->descripcion_select; ?>
-                <?php echo $controlador->inputs->alias; ?>
-                <?php echo $controlador->inputs->tg_manifiesto_id; ?>
-                <?php echo $controlador->inputs->nom_periodo_id; ?>
-
-                <div class="buttons col-md-12">
-                    <div class="col-md-6">
-                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " value="modifica">Guarda</button>
-                    </div>
-                    <div class="col-md-6 ">
-                        <a href="index.php?seccion=tg_manifiesto_periodo&accion=lista&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Lista</a>
-                    </div>
-                </div>
-            </form>
-        </div>
+    <div class="col-12 d-flex justify-content-end">
+        <button type="submit" class="btn btn-primary" name="btn_action_next">Actualizar</button>
     </div>
-</div>
+</form>
+
