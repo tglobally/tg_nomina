77c276a9d3e3b0cd92829b55ba905a4273fa0fc9
Kevin <kevin.acuna@ivitec.com.mx>
2023-08-21 17:15:14 -0500

    v0.477.74 Se actualizan campos

--- views/tg_manifiesto/alta.php
+++ views/tg_manifiesto/alta.php
@@ -1,14 +1,19 @@
 <?php /** @var \tglobally\tg_nomina\controllers\controlador_tg_manifiesto $controlador */ ?>
 
-<form class="row g-3" method="post" action="<?php echo $controlador->link_alta_bd; ?>">
-
+<form class="row g-3" method="post" action="<?php echo $controlador->link_alta_bd; ?>" enctype="multipart/form-data">
     <?php echo $controlador->inputs->com_sucursal_id; ?>
     <?php echo $controlador->inputs->org_sucursal_id; ?>
     <?php echo $controlador->inputs->tg_tipo_servicio_id; ?>
     <?php echo $controlador->inputs->fecha_inicial_pago; ?>
-    <?php echo $controlador->inputs->fecha_final_pago; ?>
     <?php echo $controlador->inputs->fecha_envio; ?>
     <?php echo $controlador->inputs->fecha_pago; ?>
+
+    <div class="control-group col-12">
+        <label class="control-label" for="archivo">Archivo Nomina</label>
+        <div class="controls">
+            <input type="file" class="form-control" id="archivo" name="archivo" multiple/>
+        </div>
+    </div>
 
     <div class="col-12 d-flex justify-content-end">
         <button type="submit" class="btn btn-primary" name="btn_action_next">Registrar</button>
