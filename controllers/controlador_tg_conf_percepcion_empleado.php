46779674ca1fe6d6b0048ce55685641da5de3336
Kevin <kevin.acuna@ivitec.com.mx>
2023-09-01 13:29:50 -0500

    v0.514.74 Se integra controlador dependiente

---
+++ controllers/controlador_tg_conf_percepcion_empleado.php
@@ -0,0 +1,6 @@
+<?php
+namespace tglobally\tg_nomina\controllers;
+class controlador_tg_conf_percepcion_empleado extends \tglobally\tg_cliente\controllers\controlador_tg_conf_percepcion_empleado {
+
+
+}
