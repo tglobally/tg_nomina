0cb046211ca24e9c2c145bc01675190afd74320f
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-09-04 10:24:41 -0600

    v0.516.74 Se ajusta controler de cont

---
+++ controllers/controlador_tg_contrato.php
@@ -0,0 +1,58 @@
+<?php
+namespace tglobally\tg_nomina\controllers;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use gamboamartin\template\html;
+use html\tg_layout_html;
+use tglobally\tg_nomina\models\tg_contrato;
+use tglobally\tg_nomina\models\tg_layout;
+use PDO;
+use stdClass;
+
+class controlador_tg_contrato extends system {
+
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
+        $modelo = new tg_contrato(link: $link);
+        $html_ = new tg_layout_html(html: $html);
+        $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
+
+        $datatables = $this->init_datatable();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar datatable', data: $datatables);
+            print_r($error);
+            die('Error');
+        }
+
+        parent::__construct(html: $html_, link: $link, modelo: $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
+        $this->seccion_titulo = 'Layout';
+        $this->titulo_pagina = "Nominas - Layout";
+        $this->titulo_accion = "Layout";
+
+        $acciones = $this->define_acciones_menu(acciones: array("alta" => $this->link_alta));
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al integrar acciones para el menu', data: $acciones);
+            print_r($error);
+            die('Error');
+        }
+    }
+
+    private function init_datatable(): stdClass
+    {
+        $columns["tg_layout_id"]["titulo"] = "Id";
+        $columns["tg_layout_descripcion"]["titulo"] = "Descripción ";
+
+        $filtro = array("tg_layout.id", "tg_layout.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+        $datatables->menu_active = true;
+
+        return $datatables;
+    }
+
+}
