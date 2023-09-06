862aab94abae73df8ccf48096dbde99603eb4b44
Kevin <kevin.acuna@ivitec.com.mx>
2023-09-06 17:13:36 -0500

    v0.540.75 Se integran elementos de configuracion

--- controllers/controlador_nom_conf_nomina.php
+++ controllers/controlador_nom_conf_nomina.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace tglobally\tg_nomina\controllers;
 
 use gamboamartin\errores\errores;
@@ -6,15 +7,17 @@ use PDO;
 use stdClass;
 use tglobally\template_tg\html;
 
-class controlador_nom_conf_nomina extends \gamboamartin\nomina\controllers\controlador_nom_conf_nomina {
+class controlador_nom_conf_nomina extends \gamboamartin\nomina\controllers\controlador_nom_conf_nomina
+{
 
-    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass())
+    {
         $html_base = new html();
-        parent::__construct( link: $link, html: $html_base);
+        parent::__construct(link: $link, html: $html_base);
 
-        $this->seccion_titulo = 'Conf. Nomina';
-        $this->titulo_pagina = "Nominas - Conf. Nomina";
-        $this->titulo_accion = "Conf. Nomina";
+        $this->seccion_titulo = "Configuraciones Nomina";
+        $this->titulo_pagina = "Nomina - Conf. Nomina";
+        $this->titulo_accion = "Listado de Configuraciones";
 
         $acciones = $this->define_acciones_menu(acciones: array("alta" => $this->link_alta));
         if (errores::$error) {
@@ -22,5 +25,23 @@ class controlador_nom_conf_nomina extends \gamboamartin\nomina\controllers\contr
             print_r($error);
             die('Error');
         }
+    }
+
+    public function init_datatable(): stdClass
+    {
+        $columns["nom_conf_nomina_id"]["titulo"] = "Id";
+        $columns["nom_conf_nomina_descripcion"]["titulo"] = "Descripción ";
+        $columns["cat_sat_periodicidad_pago_nom_descripcion"]["titulo"] = "Periodicidad Pago";
+        $columns["cat_sat_tipo_nomina_descripcion"]["titulo"] = "Tipo Nomina";
+
+        $filtro = array("nom_conf_nomina.id", "nom_conf_nomina.descripcion",
+            "cat_sat_periodicidad_pago_nom.descripcion", "cat_sat_tipo_nomina.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+        $datatables->menu_active = true;
+
+        return $datatables;
     }
 }
--- views/nom_conf_nomina/lista.php
+++ views/nom_conf_nomina/lista.php
@@ -1,3 +1,3 @@
-<div class="table-responsive">
+<div class="<!--table-responsive-->">
     <table class="table mb-0 table-striped table-sm datatable"></table>
 </div>
